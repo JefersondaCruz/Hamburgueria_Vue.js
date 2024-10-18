a67fec44c59b4cbb38b6fb23d822cf0458e43232
JefersondaCruz <jefersondacruz40@gmail.com>
2024-10-18 14:52:05 -0300

    FirstCommit

---
+++ docker-vuejs/vue-app/src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+      meta: {
+        title: 'Home'
+      }
+    },
+    {
+      path: '/pedidos',
+      name: 'Pedidos',
+      component: () => import('../views/Pedidos.vue'),
+      meta: {
+        title: 'Pedidos'
+      }
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
+});
+
+export default router
