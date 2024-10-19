465b51c412180affde5d32696737b42cfb220650
JefersondaCruz <jefersondacruz40@gmail.com>
2024-10-19 18:15:13 -0300

    feat: atualização de pedidos

--- vue-app/src/components/Dashboard.vue
+++ vue-app/src/components/Dashboard.vue
@@ -25,13 +25,13 @@
                     </ul>
                 </div>
                 <div>
-                    <select name="status" class="status">
+                    <select name="status" class="status" @change="updateBurger($event, burger.id)">
                         <option value="">Selecione</option>
-                        <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">
+                        <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo">
                             {{ s.tipo }}
                         </option>
                     </select>
-                    <button class="delete-btn">Cancelar</button>
+                    <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
                 </div>
             </div>
         </div>
@@ -71,7 +71,36 @@ export default {
             const data = await req.json();
 
             this.status = data;
+        },
 
+        async deleteBurger(id) {
+
+            const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                method: "DELETE"
+            });
+
+            const res = await req.json();
+
+            // msg
+
+            this.getPedidos();
+
+        },
+        async updateBurger(event, id) {
+
+            const option = event.target.value;
+
+            const dataJson = JSON.stringify({ status: option });
+
+            const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                method: "PATCH",
+                headers: {"Content-Type": "application/json"},
+                body: dataJson
+            });
+
+            const res = await req.json();
+
+            console.log(res);
 
         }
     },
