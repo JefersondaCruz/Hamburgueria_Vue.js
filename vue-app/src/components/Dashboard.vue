9c76c019575a39a7e5c603b8c7aadab87c887ece
JefersondaCruz <jefersondacruz40@gmail.com>
2024-10-19 17:31:08 -0300

    feat: arrumando erro no burgerform em opcionais, e resgatando os pedidos no banco

--- vue-app/src/components/Dashboard.vue
+++ vue-app/src/components/Dashboard.vue
@@ -12,15 +12,16 @@
             </div>
         </div>
         <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>jhefo</div>
-                <div>Pão de trigo</div>
-                <div>Maminha</div>
+            <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                <div class="order-number">{{ burger.id }}</div>
+                <div>{{ burger.nome }}</div>
+                <div>{{ burger.pao }}</div>
+                <div>{{ burger.carne }}</div>
                 <div>
                     <ul>
-                        <li>Salame</li>
-                        <li>Tomate</li>
+                        <li v-for="(opcional, index) in burger.opcionais" :key="index">
+                            {{opcional}}
+                        </li>
                     </ul>
                 </div>
                 <div>
@@ -31,28 +32,6 @@
                 </div>
             </div>
         </div>
-        <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>jhefo</div>
-                <div>Pão de trigo</div>
-                <div>Maminha</div>
-                <div>
-                    <ul>
-                        <li>Salame</li>
-                        <li>Tomate</li>
-                    </ul>
-                </div>
-                <div>
-                    <select name="status" class="status">
-                        <option value="">Selecione</option>
-                    </select>
-                    <button class="delete-btn">Cancelar</button>
-                </div>
-            </div>
-        </div>
-        
-        
     </div>
 
 </template>
@@ -66,6 +45,24 @@ export default {
             burger_id: null,
             status: []
         }
+    },
+    methods: {
+        async getPedidos() {
+
+            const req = await fetch("http://localhost:3000/burgers");
+
+            const data = await req.json();
+
+            this.burgers = data;
+
+            console.log(this.burgers)
+
+            // resgatar os status
+
+        }
+    },
+    mounted() {
+        this.getPedidos();
     }
 }
 </script>
